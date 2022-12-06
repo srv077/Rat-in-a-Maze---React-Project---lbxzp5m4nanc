afc3e783f8e7aa6143dc8e9825503ffe22eac438
srv077 <105067288+srv077@users.noreply.github.com>
2022-12-06 12:27:21 +0530

    spelling error

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,8 +4,8 @@ import '../Style/Header.css'
 export default function Header() {
   return (
     <div>
-      <h2>Can you block mr mouse from reacting his home?</h2>
-      <h4>You can click any cell to block ( Red color in the cell means you blocked that cell) that cell </h4>
+      <h2>Can you block mr mouse from reaching his home?</h2>
+      <h4>You can click any cell to block ( Red color in the cell means you blocked that cell ) that cell </h4>
     </div>
   )
 }
